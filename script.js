8a8dea3a2fd73b5e3024c4f7eac9a9b4e3575883
PrivacySecured <LoveNewfies@proton.me>
2025-03-19 18:58:56 -0500

    g

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-document.addEventListener('DOMContentLoaded', function(){
-    $(function(){
-        $("#menu").load("res/menu.html"); 
-    });
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Functions Pre Variable
+    function getId(element) {
+        return document.getElementById(element);
+    }
+
+    function getClass(element) {
+        return document.querySelector(`.${element}`);
+    }
+
+    function sendTo(element, path) {
+        return element.addEventListener("click", function () {
+            window.location.href = `${path}.html`
+        });
+    }
+
+    // Variables
+    const menu = getId('menu');
+
+    // Load Menu
+    if (menu) {
+        $(function () {
+            $("#menu").load("res/menu.html", function () {
+                // Once the menu is loaded, get the buttons and attach event listeners
+                const homeBtn = getId('homebtn');
+                const membershipsBtn = getId('membershipsbtn');
+                const contactBtn = getId('contactbtn');
+                const accountBtn = getId('accountbtn');
+                const loginBtn = getId('loginbtn');
+
+                // Button Redirecting
+                sendTo(homeBtn, 'index');
+                sendTo(membershipsBtn, 'memberships');
+                sendTo(contactBtn, 'contact');
+                sendTo(accountBtn, 'account');
+                sendTo(loginBtn, 'login');
+            });
+        });
+    }
 })
